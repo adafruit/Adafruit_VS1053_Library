83ba5159c705b39488887a0704f93f220f6b0215
Nitek <nitek@blickt.es>
2018-12-31 14:55:22 +0100

    make isMP3File() static

--- Adafruit_VS1053.h
+++ Adafruit_VS1053.h
@@ -172,7 +172,7 @@ class Adafruit_VS1053_FilePlayer : public Adafruit_VS1053 {
   File currentTrack;
   volatile boolean playingMusic;
   void feedBuffer(void);
-  boolean isMP3File(const char* fileName);
+  static boolean isMP3File(const char* fileName);
   unsigned long mp3_ID3Jumper(File mp3);
   boolean startPlayingFile(const char *trackname);
   boolean playFullFile(const char *trackname);
